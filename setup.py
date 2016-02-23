62b438b20d4e94876690026da691bb145c80ac84
z3ntu <luca.emanuel.weiss@gmail.com>
2016-02-23 14:47:54 +0100

    Add file & setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyPKGBUILD',
+    version='1.0',
+    packages=['pyPKGBUILD'],
+    url='https://github.com/z3ntu/pyPKGBUILD',
+    license='MIT',
+    author='Luca Weiss',
+    author_email='WEI16416@spengergasse.at',
+    description='Parses the .SRCINFO file into a python dictionary',
+)
